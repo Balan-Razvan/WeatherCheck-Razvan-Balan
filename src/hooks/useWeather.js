bee7c09fcd99ea567ddf9cd323c006d6ce827de4
razvan balan <2473254@cognizant.com>
2026-02-24 10:49:41 +0200

    useWeather hook

---
+++ src/hooks/useWeather.js
@@ -0,0 +1,39 @@
+import { useSelector, useDispatch } from "react-redux";
+import {
+    fetchWeather, fetchAirQualityData, searchLocations, setCurrentLocation, clearSearchResults, addSavedLocation, removeSavedLocation
+} from '../store/weatherSlice';
+
+export function useWeather() {
+    const dispatch = useDispatch();
+    const weather = useSelector((state) => state.waeather);
+    const {temperatureUnit, windSpeedUnit} = useSelector((state) => state.ui);
+
+    const loadForecast = (latitude, longitude) => {
+        dispatch(
+            fetchWeather({
+                latitude, longitude, options: {temperatureUnit, windSpeedUnit}
+            })
+        )
+    }
+
+    const loadAirQuality = (latitude, longitude) => {
+        dispatch(fetchAirQualityData({latitude, longitude, options:{}}))
+    }
+
+    const selectLocation = (location) => {
+        dispatch(setCurrentLocation(location));
+        loadForecast(location.latitude, location.longitude);
+        loadAirQuality(location.latitude, location.longitude);
+    }   
+
+    return {
+    ...weather,
+    loadForecast,
+    loadAirQuality,
+    selectLocation,
+    search: (query) => dispatch(searchLocations(query)),
+    clearSearch: () => dispatch(clearSearchResults()),
+    saveLocation: (location) => dispatch(addSavedLocation(location)),
+    unsaveLocation: (location) => dispatch(removeSavedLocation(location)),
+  }
+}
